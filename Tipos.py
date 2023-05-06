5d8cc4fe9047d75270a65b410e8a986e5c49b11f
Josue Fernandes <josue7mf@hotmail.com>
2023-05-05 23:34:41 -0300

    tipos basicos em python

---
+++ Tipos.py
@@ -0,0 +1,23 @@
+
+
+# Tipos em Python
+
+# inteiros
+x = 10
+print(f"{x} esse é um inteiro")
+
+# Float
+a = float(3.14)
+
+print(f"{a} esse é um float")
+
+# Bool
+
+verdade = True
+falso = False
+print(verdade and falso)
+
+# String
+mensagem = 'Olá, mundo!'
+print(mensagem.upper())
+
