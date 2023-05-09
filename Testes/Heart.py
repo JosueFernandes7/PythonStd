5c2fa712e976d895299731a92206796f34c2e0ad
Josue Fernandes <josue7mf@hotmail.com>
2023-05-09 05:00:15 -0300

    testes..

---
+++ Testes/Heart.py
@@ -0,0 +1,22 @@
+import time
+from turtle import *
+
+
+color('red','red')
+begin_fill()
+pensize(5)
+
+left(45)
+circle(50, 180)
+left(-90)
+circle(50, 190)
+
+left(-10)
+forward(100)
+
+right(85)
+forward(-100)
+
+end_fill()
+done()
+time.sleep(2)
